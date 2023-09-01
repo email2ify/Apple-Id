5101d1a6b39ba5a887f7f838f80835e9c9190151
email2ify <110549305+email2ify@users.noreply.github.com>
2023-09-01 23:35:18 +0200

    Initial

---
+++ mobile/user.py
@@ -0,0 +1,14 @@
+attempts = 6
+secret_number = 10
+
+while attempts > 0:
+
+    user_number = int(input('What is your pin number? '))
+    
+    if user_number == secret_number:
+        print('You are logged in')
+        break
+        
+    else:
+        attempts -=1
+        print(f'failed you have {attempts} attempts left')
